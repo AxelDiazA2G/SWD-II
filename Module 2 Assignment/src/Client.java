bbcea1521a48c3a1b03be0f6c848c8daa3264e07
Axel Diaz <axel.r.diaz@a2g-tech.com>
2024-01-21 10:35:03 -0500

    Corrected

--- Module 2 Assignment/src/Client.java
+++ Module 2 Assignment/src/Client.java
@@ -35,9 +35,6 @@ public class Client {
                         showItems(server);
                         break;
                     case 4:
-                        updateItem(server, scanner);
-                        break;
-                    case 5:
                         running = false;
                         break;
                     default:
@@ -61,8 +58,7 @@ public class Client {
         System.out.println("1. Add an item");
         System.out.println("2. Delete an item");
         System.out.println("3. Show list of items");
-        System.out.println("4. Update an item");
-        System.out.println("5. Quit");
+        System.out.println("4. Quit");
         System.out.print("Enter your choice: ");
     }
     
@@ -135,36 +131,5 @@ public class Client {
         System.out.println("-------------------------");
     }
     
-    /**
-     * Updates an item in the server.
-     *
-     * @param server the Server object
-     * @param scanner the Scanner object to read user input
-     */
-    private static void updateItem(Server server, Scanner scanner) {
-        System.out.println("Todo Items:");
-        System.out.println("-------------------------");
-        int index = 1;
-        for (String item : server.getItems()) {
-            System.out.println(index + ". " + item);
-            index++;
-        }
-        System.out.println("-------------------------");
-        System.out.print("Enter the index of the item to update: ");
-        if (scanner.hasNextInt()) {
-            int updateIndex = scanner.nextInt();
-            scanner.nextLine();
-            if (updateIndex >= 1 && updateIndex <= server.getItems().size()) {
-                System.out.print("Enter the new item value: ");
-                String newItem = scanner.nextLine();
-                server.updateItem(updateIndex, newItem);
-                System.out.println("Item updated successfully.");
-            } else {
-                System.out.println("Invalid index. Please enter a valid index.");
-            }
-        } else {
-            System.out.println("Invalid input. Please enter a valid index.");
-            scanner.nextLine();
-        }
-    }
+
 }
